38baa2ca375ac9f9305721fe4be6d6594682e99d
casemo <casemo@futunn.com>
2026-04-24 09:51:09 +0800

    feat: 库开发

---
+++ rslib/rslib.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      syntax: 'es2021',
+      dts: true,
+      output: {
+        distPath: { root: './dist/esm' },
+      },
+    },
+    {
+      format: 'cjs',
+      syntax: 'es2021',
+      output: {
+        distPath: { root: './dist/cjs' },
+      },
+    },
+  ],
+  output: {
+    target: 'node',
+  },
+});
